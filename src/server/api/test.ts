4574d7c8fb138cb404da6bc80471b29452d02dc3
Alejandra Coeto <a01285221@tec.mx>
2023-10-24 13:56:11 -0600

    api

---
+++ src/server/api/test.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+    try {
+        //Get body and destructure
+        const body = await req.json();
+        console.log(body, "body");
+        return new NextResponse("Registration successful", { status: 200 });
+
+    } catch (error: any) {
+        console.log(error, "Registration error");
+        return new NextResponse("Registration error", { status: 500 });
+    }
+}
