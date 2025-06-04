029e894ad136f70035f03f9b2af64cd2c908d323
Rodrigo Gamboa <rodrigo.gamboa@Outlook.com>
2025-06-04 10:41:33 -0600

    fix vulnerability on whitelisted domains for resource loading

--- next.config.mjs
+++ next.config.mjs
@@ -21,7 +21,7 @@ const config = {
 
   images: {
     remotePatterns: [
-	    { protocol: "https", hostname: "*.googleusercontent.com" },
+	    { protocol: "https", hostname: "lh3.googleusercontent.com" },
 	    { protocol: "https", hostname: "res.cloudinary.com" }
 	]
   }
