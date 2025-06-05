f7b3f2f8d59c2e108d77f2dffdb93e3520feec6d
José Luis Domínguez Morales <144069553+JLDominguezM@users.noreply.github.com>
2025-06-05 14:37:22 -0600

    SearchBar works with enter (#55)
    * Refactor SearchBar component to handle search on Enter key press and streamline button click functionality
    
    * removed redundant function
    
    ---------
    
    Co-authored-by: Gilberto Malagamba Montejo <A00839075@tec.mx>

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 
 export const SearchBar = ({
   placeHolder = "Buscar...",
@@ -8,6 +8,12 @@ export const SearchBar = ({
   setUpdateSearch: React.Dispatch<React.SetStateAction<string>>;
 }) => {
   const [text, setText] = useState("");
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setUpdateSearch(text);
+    }
+  };
 
   // https://flowbite.com/docs/forms/search-input/#search-bar-example
   return (
@@ -39,13 +45,12 @@ export const SearchBar = ({
           id="default-search"
           className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
           placeholder={placeHolder}
+          onKeyDown={handleKeyDown}
           required
         />
         <button
           type="submit"
-          onClick={() => {
-            setUpdateSearch(text);
-          }}
+          onClick={() => setUpdateSearch(text)}
           className="absolute bottom-2.5 right-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white transition duration-300 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Buscar
