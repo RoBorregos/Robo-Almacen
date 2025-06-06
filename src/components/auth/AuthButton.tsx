15907260420a4c18a5b5c32136e8f55ec1e4b683
luiss_benvenuto <luiss.benvenuto@gmail.com>
2025-06-06 13:50:05 -0600

    Update auth button to login directly via google

--- src/components/auth/AuthButton.tsx
+++ src/components/auth/AuthButton.tsx
@@ -5,7 +5,7 @@ const AuthButton: React.FC = () => {
     return (
 	<button
 	    className="font-inter mr-12 rounded-lg border bg-blue-700 px-4 py-1 text-gray-100 transition duration-300 hover:bg-blue-800"
-	    onClick={() => void signIn()}>
+	    onClick={() => void signIn("google")}>
 	    Sign in
 	</button>
     );
